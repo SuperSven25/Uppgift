0a7e374471c0616fdfcaada431e0cede401e2f0c
Alexander Svensson <alexander.svensson@learnet.se>
2022-09-16 15:44:43 +0200

    bla

---
+++ mm.js
@@ -0,0 +1,15 @@
+//var ps = require("prompt-sync");
+//var prompt = ps();
+
+//let number = parseInt(prompt("Give number: "))
+
+//let inter1=prompt("Give first interval: ")
+//let inter2=prompt("Give second interval: ")
+
+//if (((number > inter1) && (number < inter2)) ||  ((number < inter1) && (number > inter2))){
+
+    //console.log("within interval")
+//}
+//else{
+    //console.log("Not within interval")
+//}
