5e67c7f398b5a5ca004a68d3180ae43691483a4a
Alexander Svensson <alexander.svensson@learnet.se>
2022-09-22 09:31:04 +0200

    klar

--- mm.js
+++ mm.js
@@ -1,135 +1,137 @@
 //1
-//var ps = require("prompt-sync");
-//var prompt = ps();
 
-//let number = parseInt(prompt("Give number: "))
+/*
 
-//let inter1=prompt("Give first interval: ")
-//let inter2=prompt("Give second interval: ")
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//if (((number > inter1) && (number < inter2)) ||  ((number < inter1) && (number > inter2))){
+let number = parseInt(prompt("Give number: "))
+
+let inter1=prompt("Give first interval: ")
+let inter2=prompt("Give second interval: ")
+
+if (((number > inter1) && (number < inter2)) ||  ((number < inter1) && (number > inter2))){
 
     //console.log("within interval")
-//}
-//else{
+}
+else{
     //console.log("Not within interval")
-//}
+}
+*/
 
 //2
-//var ps = require("prompt-sync");
-//var prompt = ps();
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//let year = prompt("Write a year: ")
-//function leapyear(year){
-  //  if ((0 == year %4) && (0 != year % 100) || (0 == year % 400)){
-    //    console.log(year + " is a leap year");
-  //  }else {
- //       console.log(year + " is not a leap year")
- //   }
-//}
-//leapyear(year); 
-
-//3
-//var ps = require("prompt-sync");
-//var prompt = ps();
-
-//let m = prompt("Give me a month: ")
-
-//let y = prompt("Give me a year: ")
-
+let year = prompt("Write a year: ")
+function leapyear(year){
+    if ((0 == year %4) && (0 != year % 100) || (0 == year % 400)){
+        console.log(year + " is a leap year");
+    }else {
+        console.log(year + " is not a leap year")
+    }
+}
+leapyear(year); 
+*/
 
 //4
-//var ps = require("prompt-sync");
-//var prompt = ps();
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//let num1 = prompt("Give me a number: ")
-//let num2 = prompt("Give me another: ")
-//let num3 = prompt("Give me one more: ")
+let num1 = prompt("Give me a number: ")
+let num2 = prompt("Give me another: ")
+let num3 = prompt("Give me one more: ")
 
-//function greatestnumber(num1,num2,num3){
-    //if((num1>num2) && (num1>num3)){
-        //console.log(num1 + " is the greatest number of those")
-    //}else if ((num2>num1) && (num2>num3)){
+function greatestnumber(num1,num2,num3){
+    if((num1>num2) && (num1>num3)){
+        console.log(num1 + " is the greatest number of those")
+    }else if ((num2>num1) && (num2>num3)){
         //console.log(num2 + " is the greatest number of those")
-    //} else {
-     //   console.log(num3 + " is the greatest number of those")
-    //}
-//}
-//greatestnumber(num1,num2,num3);
-
+    } else {
+        console.log(num3 + " is the greatest number of those")
+    }
+}
+greatestnumber(num1,num2,num3);
+*/
 
 //5
-//var ps = require("prompt-sync");
-//var prompt = ps();
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//let num1 = prompt("Give me a number: ")
-//let num2 = prompt("Give me another: ")
-//let num3 = prompt("Give me one more: ")
+let num1 = prompt("Give me a number: ")
+let num2 = prompt("Give me another: ")
+let num3 = prompt("Give me one more: ")
 
-//function sortnumber(num1,num2,num3){
-    //if((num1>num2) && (num2>num3)){
-    //    console.log("The oder is " + num1 + num2 + num3)
-    //}
-    //else if((num1>num2) && (num3>num2)){
-    //    console.log("The oder is " + num1 + num3 + num2)
-    //}else if ((num2>num1) && (num1>num3)){
-    //    console.log("The oder is " + num2 + num1 + num3)
-    //}else if ((num2>num3) && (num3>num1)){
-    //    console.log("The oder is " + num2 + num3 + num1)
-    //}else if ((num3>num1) && (num1>num2)){
-   //     console.log("The oder is " + num3 + num1 + num2)
-    //}else {
-    //    console.log("The oder is " + num3 + num2 + num1)
-    //}
-//}
-//sortnumber(num1,num2,num3)
-
+function sortnumber(num1,num2,num3){
+    if((num1>num2) && (num2>num3)){
+       console.log("The oder is " + num1 + num2 + num3)
+    }
+    else if((num1>num2) && (num3>num2)){
+        console.log("The oder is " + num1 + num3 + num2)
+    }else if ((num2>num1) && (num1>num3)){
+        console.log("The oder is " + num2 + num1 + num3)
+    }else if ((num2>num3) && (num3>num1)){
+        console.log("The oder is " + num2 + num3 + num1)
+    }else if ((num3>num1) && (num1>num2)){
+       console.log("The oder is " + num3 + num1 + num2)
+    }else {
+        console.log("The oder is " + num3 + num2 + num1)
+    }
+}
+sortnumber(num1,num2,num3)
+*/
 
 //6
-//var ps = require("prompt-sync");
-//var prompt = ps();
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//let num1 = prompt("Give me a number: ")
-//let num2 = prompt("Give me another: ")
-//let num3 = prompt("Give me one more: ")
+let num1 = prompt("Give me a number: ")
+let num2 = prompt("Give me another: ")
+let num3 = prompt("Give me one more: ")
 
-//function goupordown(num1,num2,num3){
-    //if((num1>num2) && (num2>num3)){
-   //     console.log("decrease")
-   // }else if((num3>num2) && (num2>num1)){
-   //     console.log("increase")
-   // }else{
-  //      console.log("neither")
-  //  }
-//}
-//goupordown(num1,num2,num3)
-
+function goupordown(num1,num2,num3){
+    if((num1>num2) && (num2>num3)){
+        console.log("decrease")
+    }else if((num3>num2) && (num2>num1)){
+        console.log("increase")
+    }else{
+        console.log("neither")
+    }
+}
+goupordown(num1,num2,num3)
+*/
 
 // 1
 
-//var ps = require("prompt-sync");
-//var prompt = ps();
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
 
-//let bert = parseInt(prompt("Give me a number: "))
+let bert = parseInt(prompt("Give me a number: "))
 
-//function plusminus(bert){
+function plusminus(bert){
 
-//if(bert>0){
-//    console.log("It is posetive")
+if(bert>0){
+    console.log("It is posetive")
 
-//}else if(bert<0){
-//    console.log("It is negative")
-//}else{
-//    console.log("It is 0")
-//}
+}else if(bert<0){
+    console.log("It is negative")
+}else{
+   console.log("It is 0")
+}
 
-//}
-//plusminus(bert)
-
+}
+plusminus(bert)
+*/
 
 
 // 2
 
+/*
 var ps = require("prompt-sync");
 var prompt = ps();
 
@@ -138,31 +140,94 @@ let hum = prompt("Give me a symbol: ")
 
 if (hum == "a"){
    console.log("Letter")
-}if (hum == "b"){
+}else if (hum == "b"){
     console.log("Letter")
- }if (hum == "c"){
+ }else if (hum == "c"){
     console.log("Letter")
- }if (hum == "d"){
+ }else if (hum == "d"){
     console.log("Letter")
- }if (hum == "e"){
+ }else if (hum == "e"){
     console.log("Letter")
-}if (hum == "f"){
+}else if (hum == "f"){
     console.log("Letter")
- }if (hum == "g"){
+ }else if (hum == "g"){
     console.log("Letter")
- }if (hum == "h"){
+ }else if (hum == "h"){
     console.log("Letter")
- }if (hum == "i"){
+ }else if (hum == "i"){
     console.log("Letter")
- } if (hum == "j"){
+ }else if (hum == "j"){
     console.log("Letter")
- }if (hum == "k"){
+ }else if (hum == "k"){
     console.log("Letter")
- }if (hum == "l"){
+ }else if (hum == "l"){
     console.log("Letter")
  }
  
  else{
-    console.log ("not a letter")
+   console.log ("not a letter")
  }
- 
+ */
+
+ //3
+/*
+ var ps = require("prompt-sync");
+var prompt = ps();
+
+
+let letter = prompt("Ge mig en bokstav: ")
+
+if(letter == "a"){
+   console.log("De e en vokal")
+}else if (letter == "o"){
+   console.log("De e en vokal")
+}else if (letter == "u"){
+   console.log("De e en vokal")
+}else if (letter == "å"){
+   console.log("De e en vokal")
+}else if (letter == "e"){
+   console.log("De e en vokal")
+}else if (letter == "i"){
+   console.log("De e en vokal")
+}else if (letter == "y"){
+   console.log("De e en vokal")
+}else if (letter == "ä"){
+   console.log("De e en vokal")
+}else if (letter == "ö"){
+   console.log("De e en vokal")
+}else{
+   console.log("De e en konsonat")
+}
+*/
+
+// 4
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let num1 = parseInt(prompt("Numeber plz: "))
+let num2 = parseInt(prompt("Numeber plz: "))
+let num3 = parseInt(prompt("Numeber plz: "))
+
+if(num1 + num2 + num3 === 180){
+   console.log("It's a triangle")
+}else{
+   console.log("Not triangle")
+}
+*/
+
+// 6
+
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let num = parseInt(prompt("Ge mig nummer: "))
+
+
+if((num % 2 == 0)){
+   console.log("De e jämnt")
+}else {
+   console.log("De e udda")
+}
+*/
---
+++ test.js
@@ -0,0 +1,12 @@
+var number = 4
+
+console.log(number % 2)
+
+/*
+
+4/2 = 2
+4/3 = 1
+
+8%2 = 1
+
+*/
